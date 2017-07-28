f19aba9e49cd84325979aa499d4517b52af0b1b3
ciberkleid <ciberkleid@gmail.com>
2017-07-28 10:45:20 -0400

    Simplified script

--- 01_cleanup.sh
+++ 01_cleanup.sh
@@ -1,21 +1,10 @@
-# Get user input:
-echo "Delete apps? [Y]: "
-read APPS
-APPS=${APPS:-Y}
-
-echo "Delete services? [Y]: "
+echo "This script will delete the greeting-ui and fortune-service apps."
+echo "Delete services too? [Y]: "
 read SERVICES
 SERVICES=${SERVICES:-Y}
 
-#currentSpace=`cf target | grep "space:" | cut -c 17-${lastIndex}`
-#echo "Delete current space '$currentSpace'? [N]: "
-#read SPACE
-#SPACE=${SPACE:-N}
-
-if [[ $APPS == "Y" ]]; then
-  cf delete greeting-ui -f -r
-  cf delete fortune-service -f -r
-fi
+cf delete greeting-ui -f -r
+cf delete fortune-service -f -r
 
 if [[ $SERVICES == "Y" ]]; then
   cf delete-service config-server -f
@@ -23,15 +12,3 @@ if [[ $SERVICES == "Y" ]]; then
   cf delete-service service-registry -f
   cf delete-service circuit-breaker-dashboard -f
 fi
-
-#echo "Continuing will delete EVERYTHING in this space. Continue? [Y]: "
-#read DELETE
-#DELETE=${DELETE:-Y}
-
-#if [[ $DELETE == "Y" ]]; then
-#  cf delete-space -f $SPACE
-#  cf create-space $SPACE
-#  cf target -s $SPACE
-#else 
-#  echo "Space cleanup aborted"
-#fi
