c3acc382e02d4d74f167ce3c67cf4bfc4d3e6615
Cora Iberkleid <ciberkleid@pivotal.io>
2019-04-24 11:11:40 -0400

    push distro

--- cloud-pipelines-push-distro.sh
+++ cloud-pipelines-push-distro.sh
@@ -1,4 +1,4 @@
-git archive -o cloud-pipelines/dist/cloud-pipelines-ext.tar.gz HEAD:cloud-pipelines
+git archive -o cloud-pipelines/dist/cloud-pipelines-ext.tar.gz HEAD:cloud-pipelines/ext
 git add cloud-pipelines/dist/cloud-pipelines-ext.tar.gz
 git commit -m "pushing distro"
 git push
