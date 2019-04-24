5582c295dea77cc99cc281c77821f29062d54001
Cora Iberkleid <ciberkleid@pivotal.io>
2019-04-24 12:06:08 -0400

    reformat comments

--- cloud-pipelines/bootstrap.sh
+++ cloud-pipelines/bootstrap.sh
@@ -4,7 +4,8 @@
 # This will download the Cloud Pipelines source as well as the custom extensions source
 # This will also source files from both downloads to set up the environment
 
-echo -e "\n\n########## ---------- Set up Cloud Pipelines environment ---------- ##########"
+echo -e "\n\n########## ---------- Set up Cloud Pipelines & Extensions Environment ---------- ##########"
+echo -e "\n\n########## Set up Cloud Pipelines environment ##########"
 rm -rf .git/tools && mkdir -p .git/tools && cd "${WORKSPACE}"/.git/tools && curl -Lk "https://github.com/CloudPipelines/scripts/raw/master/dist/scripts.tar.gz" -o pipelines.tar.gz && tar xf pipelines.tar.gz --strip-components 1 && cd "${WORKSPACE}"
 
 if [[ -z $(which ruby) ]]; then
@@ -19,7 +20,7 @@ export CI=Jenkins
 
 source "${WORKSPACE}"/.git/tools/src/main/bash/pipeline.sh
 
-echo -e "\n\n########## ---------- Set up Cloud Pipelines Extensions ---------- ##########"
+echo -e "\n\n########## Set up Cloud Pipelines Extensions ##########"
 rm -rf .git/tools-ext && mkdir -p .git/tools-ext && cd "${WORKSPACE}"/.git/tools-ext && curl -Lk "https://github.com/ciberkleid/cna-demo-setup/raw/master/cloud-pipelines/dist/cloud-pipelines-ext.tar.gz" -o pipelines-ext.tar.gz && tar xf pipelines-ext.tar.gz && cd "${WORKSPACE}"
 
 export WORKSPACE_EXT="${WORKSPACE}/.git/tools-ext/custom"
@@ -30,4 +31,4 @@ source "${WORKSPACE_EXT}"/init-env.sh
 
 find "${WORKSPACE_EXT}" -type f -iname "*.sh" -exec chmod +x {} \;
 
-echo -e "\n\n########## ---------- End Cloud Pipelines & Extensions setup ---------- ##########"
+echo -e "\n\n########## ---------- End Cloud Pipelines & Extensions Environment Setup ---------- ##########"
