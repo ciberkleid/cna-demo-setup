9fdaf128d6782694fd1a1f74d7ba948d32881c69
Cora Iberkleid <ciberkleid@pivotal.io>
2019-04-24 21:33:46 -0400

    added comments

--- cloud-pipelines/bootstrap.sh
+++ cloud-pipelines/bootstrap.sh
@@ -10,6 +10,13 @@
 echo -e "\n\n########## Run common scripts ##########"
 
 rm -rf .git/tools-ext && mkdir -p .git/tools-ext && cd "${WORKSPACE}"/.git/tools-ext && curl -Lk "https://github.com/ciberkleid/cna-demo-setup/raw/master/cloud-pipelines/dist/cloud-pipelines-ext.tar.gz" -o pipelines-ext.tar.gz && tar xf pipelines-ext.tar.gz && cd "${WORKSPACE}"
+
+# Overwrite default values by setting M2_SETTINGS_REPO_USERNAME and
+# M2_SETTINGS_REPO_ROOT as apporpriate for your Bintray account
+# Se the custom/init-env.sh file for the default values
+#export M2_SETTINGS_REPO_USERNAME=<your bintray username>
+#export M2_SETTINGS_REPO_ROOT=<your bintray maven repo name>
+
 source "${WORKSPACE}"/.git/tools-ext/custom/init-env.sh
 
 echo -e "\n\n########## Run job script ##########"
