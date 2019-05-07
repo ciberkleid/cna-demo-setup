95fd376c49a45a38d7b7419897dbdc0d3fb76a0b
Cora Iberkleid <ciberkleid@pivotal.io>
2019-05-06 20:46:33 -0400

    added comments

--- cloud-pipelines/admin/_demo_env.sh
+++ cloud-pipelines/admin/_demo_env.sh
@@ -4,11 +4,20 @@ set -o errexit
 set -o errtrace
 set -o pipefail
 
+########## fortune-service commits ##########
+# Baseline
 export FORTUNE_SERVICE_1=9d944fa4e5d264e95446702ebb2c22c0ba13ce52
-export FORTUNE_SERVICE_2_SNAPSHOT=3f999a6f502458abc96491441dadceeb9ea92e9e
-export FORTUNE_SERVICE_2=9cc2b7470abb7bd1f6779278ea49fb5c97703eb6
-export FORTUNE_SERVICE_3_SNAPSHOT=eb885ffbf81369f46ea726476314eeb803c72749
-export FORTUNE_SERVICE_3=a654189fd5e53f14adbe86193dbee393b1f846fd
+# Breaking API change
+export FORTUNE_SERVICE_2=3f999a6f502458abc96491441dadceeb9ea92e9e
+# Back-compatible API change
+export FORTUNE_SERVICE_3=9cc2b7470abb7bd1f6779278ea49fb5c97703eb6
+# Breaking DB schema change
+export FORTUNE_SERVICE_4=eb885ffbf81369f46ea726476314eeb803c72749
+# Back-compatible DB schema change
+export FORTUNE_SERVICE_5=a654189fd5e53f14adbe86193dbee393b1f846fd
 
+########## greeting-ui commits ##########
+# Baseline
 export GREETING_UI_1=bcd72b4df684817499d2238ca0a3085f16d685ee
-export GREETING_UI_2=
+# Cross-compatible API change
+export GREETING_UI_2=afcda628abab7c0a979d00d944e08f2b997b06be
