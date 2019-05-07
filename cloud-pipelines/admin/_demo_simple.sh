7eee7bb6e1dbef04a1129733f345ae8d9887bbd4
Cora Iberkleid <ciberkleid@pivotal.io>
2019-05-06 20:49:11 -0400

    updated variables names

--- cloud-pipelines/admin/_demo_simple.sh
+++ cloud-pipelines/admin/_demo_simple.sh
@@ -17,11 +17,13 @@ DEMO_TEMP="${savedPWD}"/_demo_temp
 echo -e "\n########## DEMO CONTROLLER ##########"
 echo -e "\nChoose an option? [1-5]: "
 echo "1 - Clone repos"
-echo "2 - Build fortune-service-1           (working baseline)"
-echo "3 - Test fortune-service-2-snapshot   (breaking api change)"
-echo "4 - Build fortune-service-2           (back-compatible api change)"
-echo "5 - Test fortune-service-3-snapshot   (breaking db change)"
-echo "6 - Build fortune-service-3           (back-compatible api and db changes)"
+echo "2 - Build fortune-service-build-1         (working baseline)"
+echo "3 - Test fortune-service-build-2          (breaking api change)"
+echo "4 - Build fortune-service-build-3         (back-compatible api change)"
+echo "5 - Test fortune-service-build-4          (breaking db change)"
+echo "6 - Build fortune-service-build-5         (back-compatible db change)"
+echo "7 - Build greeting-ui-build-1             (test with stub for fortune-service-build-1)"
+echo "8 - Build greeting-ui-build-2             (test with stubs for fortune-service-build-1, fortune-service-build-3 and fortune-service-build-5)"
 echo "9 - Exit"
 read choice
 
@@ -31,112 +33,123 @@ case ${choice} in
         mkdir -p "${DEMO_TEMP}"
         cd "${DEMO_TEMP}"
 
-        git clone https://github.com/ciberkleid/fortune-service.git fortune-service-1
-        cp -r fortune-service-1 fortune-service-2-snapshot
-        cp -r fortune-service-1 fortune-service-2
-        cp -r fortune-service-1 fortune-service-3-snapshot
-        cp -r fortune-service-1 fortune-service-3
+        git clone https://github.com/ciberkleid/fortune-service.git fortune-service-build-1
+        cp -r fortune-service-build-1 fortune-service-build-2
+        cp -r fortune-service-build-1 fortune-service-build-3
+        cp -r fortune-service-build-1 fortune-service-build-4
+        cp -r fortune-service-build-1 fortune-service-build-5
 
-        git clone https://github.com/ciberkleid/greeting-ui.git greeting-ui-1
-        #cp -r greeting-ui-blue greeting-ui-2
+        git clone https://github.com/ciberkleid/greeting-ui.git greeting-ui-build-1
+        cp -r greeting-ui-build-1 greeting-ui-build-2
 
-        cd fortune-service-1
+        cd fortune-service-build-1
         git checkout ${FORTUNE_SERVICE_1} || { echo >&2 "git checkout failed with $?"; return 1; }
-        ./mvnw versions:set -DnewVersion="fortune-service-1" -DprocessAllModules --quiet
+        ./mvnw versions:set -DnewVersion="build-1" -DprocessAllModules --quiet
         cd ..
-        cd fortune-service-2-snapshot
-        git checkout ${FORTUNE_SERVICE_2_SNAPSHOT} || { echo >&2 "git checkout failed with $?"; return 1; }
-        ./mvnw versions:set -DnewVersion="fortune-service-2-snapshot" -DprocessAllModules --quiet
-        cd ..
-        cd fortune-service-2
+        cd fortune-service-build-2
         git checkout ${FORTUNE_SERVICE_2} || { echo >&2 "git checkout failed with $?"; return 1; }
-        ./mvnw versions:set -DnewVersion="fortune-service-2" -DprocessAllModules --quiet
+        ./mvnw versions:set -DnewVersion="build-2" -DprocessAllModules --quiet
         cd ..
-        cd fortune-service-3-snapshot
-        git checkout ${FORTUNE_SERVICE_3_SNAPSHOT} || { echo >&2 "git checkout failed with $?"; return 1; }
-        ./mvnw versions:set -DnewVersion="fortune-service3-snapshot" -DprocessAllModules --quiet
-        cd ..
-        cd fortune-service-3
+        cd fortune-service-build-3
         git checkout ${FORTUNE_SERVICE_3} || { echo >&2 "git checkout failed with $?"; return 1; }
-        ./mvnw versions:set -DnewVersion="fortune-service-3" -DprocessAllModules --quiet
+        ./mvnw versions:set -DnewVersion="build-3" -DprocessAllModules --quiet
+        cd ..
+        cd fortune-service-build-4
+        git checkout ${FORTUNE_SERVICE_4} || { echo >&2 "git checkout failed with $?"; return 1; }
+        ./mvnw versions:set -DnewVersion="build-4" -DprocessAllModules --quiet
+        cd ..
+        cd fortune-service-build-5
+        git checkout ${FORTUNE_SERVICE_5} || { echo >&2 "git checkout failed with $?"; return 1; }
+        ./mvnw versions:set -DnewVersion="build-5" -DprocessAllModules --quiet
         cd ..
 
-        cd greeting-ui-1
+        cd greeting-ui-build-1
         git checkout ${GREETING_UI_1} || { echo >&2 "git checkout failed with $?"; return 1; }
-        ./mvnw versions:set -DnewVersion="greeting-ui-1" -DprocessAllModules --quiet
+        ./mvnw versions:set -DnewVersion="greeting-ui-build-1" -DprocessAllModules --quiet
         cd ..
-        #cd greeting-ui-2
-        #git checkout ${GREETING_UI_2} || { echo >&2 "git checkout failed with $?"; return 1; }
-        #./mvnw versions:set -DnewVersion="greeting-ui-2" -DprocessAllModules --quiet
-        #cd ..
+        cd greeting-ui-build-2
+        git checkout ${GREETING_UI_2} || { echo >&2 "git checkout failed with $?"; return 1; }
+        ./mvnw versions:set -DnewVersion="greeting-ui-build-2" -DprocessAllModules --quiet
+        cd ..
         ;;
-    2) echo -e "\nBuilding fortune-service-1 (baseline))"
-        cd fortune-service-1
+    2) echo -e "\nBuilding fortune-service-build-1 (baseline)"
+        cd fortune-service-build-1
         echo -e "\nInitial release - no API or DB back compatibility checks needed. Expecting success."
-        rm -rf ~/.m2/repository/io/pivotal/fortune-service/fortune-service-1
+        rm -rf ~/.m2/repository/io/pivotal/fortune-service/fortune-service-build-1
         ./mvnw clean install
-        echo -e "\nFinished building fortune-service-1"
+        echo -e "\nFinished building fortune-service-build-1"
         echo "Generated files in local m2 repo:"
-        ls ~/.m2/repository/io/pivotal/fortune-service/fortune-service-1
+        ls ~/.m2/repository/io/pivotal/fortune-service/fortune-service-build-1
         cd ..
         ;;
-    3) echo -e "\nTesting fortune-service-2-snapshot (breaking api change)"
-        cd fortune-service-2-snapshot
-        echo -e "\nTest fortune-service-2-snapshot. Expecting success."
+    3) echo -e "\nTesting fortune-service-build-2 (breaking api change)"
+        cd fortune-service-build-2
+        echo -e "\nTest fortune-service-build-2. Expecting success."
         ./mvnw clean test
-        echo -e "\nCheck API back compatibility against fortune-service-1. Expecting failure due to known back compatibility issues."
-        ./mvnw clean verify -Papicompatibility -Dlatest.production.version=1 -Dcontracts.mode=LOCAL
-        echo -e "\nFinished testing fortune-service-2-snapshot"
+        echo -e "\nCheck API back compatibility against fortune-service-build-1. Expecting failure due to known back compatibility issues."
+        ./mvnw clean verify -Papicompatibility -Dlatest.production.version=build-1 -Dcontracts.mode=LOCAL
+        echo -e "\nFinished testing fortune-service-build-2"
         cd ..
         ;;
-    4) echo -e "\nBuilding fortune-service-2 (back-compatible api change)"
-        cd fortune-service-2
-        echo -e "\nCheck API back compatibility against fortune-service-1. Expecting success."
-        ./mvnw clean verify -Papicompatibility -Dlatest.production.version=1 -Dcontracts.mode=LOCAL
-         echo -e "\nBuild fortune-service-2. Expecting success."
+    4) echo -e "\nBuilding fortune-service-build-3 (back-compatible api change)"
+        cd fortune-service-build-3
+        echo -e "\nCheck API back compatibility against fortune-service-build-1. Expecting success."
+        ./mvnw clean verify -Papicompatibility -Dlatest.production.version=build-1 -Dcontracts.mode=LOCAL
+         echo -e "\nBuild fortune-service-build-3. Expecting success."
         ./mvnw clean install
-        echo -e "\nFinished building fortune-service-2"
+        echo -e "\nFinished building fortune-service-build-3"
         echo "Generated files in local m2 repo:"
-        ls ~/.m2/repository/io/pivotal/fortune-service/fortune-service-2
+        ls ~/.m2/repository/io/pivotal/fortune-service/fortune-service-build-3
         cd ..
         ;;
-    5) echo -e "\nTesting fortune-service-3-snapshot (breaking db change)"
-        cd fortune-service-3-snapshot
-        echo -e "\nTest fortune-service-3-snapshot. Expecting success."
+    5) echo -e "\nTesting fortune-service-build-4 (breaking db change)"
+        cd fortune-service-build-4
+        echo -e "\nTest fortune-service-build-4. Expecting success."
         ./mvnw clean test
-        echo -e "\nCheck DB back compatibility against fortune-service-2. Expecting failure due to known back compatibility issues."
-        cd ../fortune-service-2
-        ./mvnw clean test -Dspring.flyway.locations=filesystem:../fortune-service-3-snapshot/src/main/resources/db/migration
+        echo -e "\nCheck DB back compatibility against fortune-service-build-3. Expecting failure due to known back compatibility issues."
+        cd ../fortune-service-build-3
+        ./mvnw clean test -Dspring.flyway.locations=filesystem:../fortune-service-build-4/src/main/resources/db/migration
         cd ..
-        echo -e "\nFinished testing fortune-service-3-snapshot"
+        echo -e "\nFinished testing fortune-service-build-4"
         cd ..
         ;;
-    6) echo -e "\nTesting fortune-service-3 (back-compatible api and db changes)"
-        cd fortune-service-3
-        echo -e "\nCheck API back compatibility against fortune-service-2. Expecting success."
-        ./mvnw clean verify -Papicompatibility -Dlatest.production.version=2 -Dcontracts.mode=LOCAL
-        echo -e "\nCheck API back compatibility against fortune-service-1. Expecting success."
-        ./mvnw clean verify -Papicompatibility -Dlatest.production.version=1 -Dcontracts.mode=LOCAL
+    6) echo -e "\nTesting fortune-service-build-5 (back-compatible api and db changes)"
+        cd fortune-service-build-5
+        echo -e "\nCheck API back compatibility against fortune-service-build-3. Expecting success."
+        ./mvnw clean verify -Papicompatibility -Dlatest.production.version=build-3 -Dcontracts.mode=LOCAL
+        echo -e "\nCheck API back compatibility against fortune-service-build-1. Expecting success."
+        ./mvnw clean verify -Papicompatibility -Dlatest.production.version=build-1 -Dcontracts.mode=LOCAL
 
-        echo -e "\nCheck DB back compatibility against fortune-service-2. Expecting success."
-        cd ../fortune-service-2
-        ./mvnw clean test -Dspring.flyway.locations=filesystem:../fortune-service-3-snapshot/src/main/resources/db/migration
+        echo -e "\nCheck DB back compatibility against fortune-service-build-3. Expecting success."
+        cd ../fortune-service-build-3
+        ./mvnw clean test -Dspring.flyway.locations=filesystem:../fortune-service-build-5/src/main/resources/db/migration
         cd ..
-        echo -e "\nCheck DB back compatibility against fortune-service-1. Expecting success."
-        cd ../fortune-service-1
-        ./mvnw clean test -Dspring.flyway.locations=filesystem:../fortune-service-3-snapshot/src/main/resources/db/migration
+        echo -e "\nCheck DB back compatibility against fortune-service-build-1. Expecting success."
+        cd ../fortune-service-build-1
+        ./mvnw clean test -Dspring.flyway.locations=filesystem:../fortune-service-build-5/src/main/resources/db/migration
         cd ..
-        echo -e "\nBuild fortune-service-3. Expecting success."
+        echo -e "\nBuild fortune-service-build-5. Expecting success."
         ./mvnw clean install
-        echo -e "\nFinished building fortune-service-3"
+        echo -e "\nFinished building fortune-service-build-5"
         echo "Generated files in local m2 repo:"
-        ls ~/.m2/repository/io/pivotal/fortune-service/fortune-service-3
+        ls ~/.m2/repository/io/pivotal/fortune-service/fortune-service-build-5
+        cd ..
+        ;;
+    7) echo -e "\Building greeting-ui-build-1"
+        cd greeting-ui-build-1
+        echo -e "\Building greeting-ui-build-1. Testing against fortune-service-build-1 stub. Expected success"
+        export STUBS=io.pivotal:fortune-service:build-1; ./mvnw clean test -Dstubrunner.stubs-mode=LOCAL
+        echo -e "\nFinished building greeting-ui-build-1"
+        ls ~/.m2/repository/io/pivotal/greeting-ui/greeting-ui-build-1
+        cd ..
+        ;;
+    8) echo -e "\Building greeting-ui-build-2"
+        cd greeting-ui-build-2
+        echo -e "\Building greeting-ui-build-2. Testing against fortune-service-build-1, fortune-service-build-3 and fortune-service-build-5 stubs. Expected success"
+        export STUBS=io.pivotal:fortune-service:build-1,io.pivotal:fortune-service:build-3,io.pivotal:fortune-service:build-5; ./mvnw clean test -Dstubrunner.stubs-mode=LOCAL
+        echo -e "\nFinished building greeting-ui-build-2"
+        ls ~/.m2/repository/io/pivotal/greeting-ui/greeting-ui-build-2
         cd ..
         ;;
     *) exit ;;
 esac
-
-
-
-
-
