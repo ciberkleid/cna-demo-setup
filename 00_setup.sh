3ecda2be15bcea8633b8b9684bc9072bd43da92d
Cora Iberkleid <ciberkleid@pivotal.io>
2019-05-28 16:23:23 -0400

    single setup file

--- 00_setup.sh
+++ 00_setup.sh
@@ -1,37 +1,52 @@
-# Get user input:
+#!/usr/bin/env bash
+
+# Get user input & set variables
 echo "Enter the Config Server URI [https://github.com/ciberkleid/app-config]: "
 read GIT_URI
-echo "Enable C2C Networking? [N]: "
-read C2C
 echo "Build apps? [N]: "
 read BUILD
-echo "Use random routes? [Y]: "
-read RAND_ROUTES
+echo "Enable C2C Networking? [eureka|mesh|NONE]: "
+read C2C
 
-# Set variables
-CF_API=`cf api | head -1 | cut -c 25-`
+CF_API=`cf api | head -1 | cut -c 17-`
 GIT_URI=${GIT_URI:-https://github.com/ciberkleid/app-config}
-C2C=${C2C:-N}
 BUILD=${BUILD:-N}
-RAND_ROUTES=${RAND_ROUTES:-Y}
+case ${C2C} in
+    e|eureka)
+        C2C=Y
+        EUREKA=Y
+        MANIFEST=manifest-c2c-eureka.yml
+    ;;
+    m|mesh)
+        C2C=Y
+        EUREKA=N
+        MANIFEST=manifest-c2c-mesh.yml
+    ;;
+    *)  C2C=N
+        EUREKA=Y
+        MANIFEST=manifest.yml
+    ;;
+esac
 
 # Create Config Server JSON Config File
 echo "{\"git\": {\"uri\": \"${GIT_URI}\"}}" > cloud-config-uri.json
 
 # Create services
+SCS_PLAN=standard
 if [[ $CF_API == *"api.run.pivotal.io"* ]]; then
-  cf create-service p-config-server trial config-server -c cloud-config-uri.json
-  cf create-service p-service-registry trial service-registry
-  cf create-service p-circuit-breaker-dashboard trial circuit-breaker-dashboard
+  SCS_PLAN=trial
   cf create-service cloudamqp lemur cloud-bus
   cf create-service cleardb spark fortune-db
 else
-  cf create-service p-config-server standard config-server -c cloud-config-uri.json
-  cf create-service p-service-registry standard service-registry
-  cf create-service p-circuit-breaker-dashboard standard circuit-breaker-dashboard
   cf create-service p.rabbitmq single-node-3.7 cloud-bus
   cf create-service p.mysql db-small fortune-db
 fi
+cf create-service p-config-server $SCS_PLAN config-server -c cloud-config-uri.json
+cf create-service p.config-server $SCS_PLAN config-server -c cloud-config-uri.json
+cf create-service p-circuit-breaker-dashboard $SCS_PLAN circuit-breaker-dashboard
+if [[ $EUREKA == "Y" ]]; then
+  cf create-service p-service-registry $SCS_PLAN service-registry
+fi 
 
 # Build apps
 if [[ $BUILD == "Y" ]]; then
@@ -57,28 +72,19 @@ fi
 echo "Service initialization - successful"
 
 # Push apps
-if [[ $RAND_ROUTES == "Y" ]]; then
-  cd ../fortune-service
-  cf push --no-start --random-route
-  cd ../greeting-ui
-  cf push --no-start --random-route
-  cd ../cna-demo-setup
-else
-  cd ../fortune-service
-  cf push --no-start
-  cd ../greeting-ui
-  cf push --no-start
-  cd ../cna-demo-setup
-fi
+cd ../fortune-service
+cf push -f $MANIFEST --no-start
+cd ../greeting-ui
+cf push -f $MANIFEST --no-start
+cd ../cna-demo-setup
 
-# Set env variables and optionally enable c2c access
+cf set-env fortune-service TRUST_CERTS $CF_API
+cf set-env fortune-service TRUST_CERTS $CF_API
+
 if [[ $C2C == "Y" ]]; then
-  cf set-env fortune-service SPRING_PROFILES_ACTIVE c2c
-  cf set-env greeting-ui SPRING_PROFILES_ACTIVE c2c
   cf add-network-policy greeting-ui --destination-app fortune-service --protocol tcp --port 8080
 fi
 
 # Start apps
-cf start fortune-service
-cf start greeting-ui
-
+cf restage fortune-service
+cf restage greeting-ui
